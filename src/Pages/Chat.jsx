e3001360450c78fd028f948f051dd4aa6bb15d45
SheheryarGhaznavi <sheheryarghaznavi@yahoo.com>
2023-08-24 01:07:46 +0500

    chat page completed

--- src/Pages/Chat.jsx
+++ src/Pages/Chat.jsx
@@ -1,5 +1,21 @@
-const Chat = () => {
-    return <>chats</>;
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from "react-chat-engine-advanced";
+
+
+const Chat = (props) => {
+
+    const chatProps = useMultiChatLogic(
+        "187447ea-6410-45bf-916c-240b986f99db",
+        props.user.username,
+        props.user.secret
+    );
+
+    return (
+
+        <div style={{height: '100vh'}}>
+            <MultiChatSocket { ...chatProps } />
+            <MultiChatWindow { ...chatProps } style={{height: '100%'}} />
+        </div>
+    )
 }
 
 export default Chat
